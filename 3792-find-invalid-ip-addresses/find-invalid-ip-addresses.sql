290bf649a44d0132ab4468dfde8e211381b3f3b4
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2025-11-20 14:43:37 -0500

    Time: 303 ms (39.16%) | Memory: 0B (100.00%) - LeetSync

--- 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
+++ 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-with grouped_logs as
-(
-    select ip, count(ip) as invalid_count
-    from logs
-    group by ip
-),
+with 
 cte as(
 select *,
 SUBSTRING_INDEX(ip, '.', 1) AS part1,
@@ -12,10 +7,10 @@ SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1) AS part2,
 SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1) AS part3,
 SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 4), '.', -1) AS part4,
 (LENGTH(ip) - LENGTH(REPLACE(ip, '.', ''))) AS dot_count
-from grouped_logs)
+from logs)
 
 select 
-ip, invalid_count
+ip, count(ip) as invalid_count
 from cte
 where part1>255 or part2>255 or part3>255 or part4>255 or
    LEFT(part1, 1) = 0
@@ -23,5 +18,6 @@ where part1>255 or part2>255 or part3>255 or part4>255 or
     OR LEFT(part3, 1) = 0
     OR LEFT(part4, 1) = 0 
 #part1 like('0%') or part2 like('0%') or part3 like('0%') or part4 like('0%')
-or dot_count!=3
+or dot_count<>3
+group by ip
 order by  invalid_count desc,ip desc;
