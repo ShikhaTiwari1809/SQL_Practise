cdc7546bc2745c9fab908453fb506050ce27afe1
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2025-11-20 15:02:36 -0500

    Time: 299 ms (50.10%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 
-select user_id, round(trial_avg_duration,2) as trial_avg_duration, round(paid_avg_duration,2) as paid_avg_duration from(
+
 select user_id,
-sum(case when activity_type ='free_trial' then activity_duration else 0 end)/sum(case when activity_type ='free_trial' then 1 else 0 end) as trial_avg_duration,
-sum(case when activity_type ='paid' then activity_duration else 0 end)/sum(case when activity_type ='paid' then 1 else 0 end) as paid_avg_duration
+ROUND(sum(case when activity_type ='free_trial' then activity_duration else 0 end)/sum(case when activity_type ='free_trial' then 1 else 0 end),2) as trial_avg_duration,
+ROUND(sum(case when activity_type ='paid' then activity_duration else 0 end)/sum(case when activity_type ='paid' then 1 else 0 end),2) as paid_avg_duration
 from UserActivity
-group by user_id)p
-where paid_avg_duration is not null
+group by user_id
+having paid_avg_duration is not null
 order by user_id
